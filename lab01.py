08637e6f1213a7402651fcacf0e181694c318c31
Kresna Satya <kresnasatya72@gmail.com>
2025-10-27 13:50:44 +0800

    Chapter 1: Downloading Web Pages - Request and Response

--- lab01.py
+++ lab01.py
@@ -15,3 +15,26 @@ class URL:
             family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
         )
         s.connect((self.host, 80))
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "\r\n"
+        s.send(request.encode("utf8"))
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        statusline = response.readline()
+        version, status, explaination = statusline.split(" ", 2)
+        # just curious what's the actual value I get here
+        print("version: ", version)
+        print("status: ", status)
+        print("explaination: ", explaination)
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+        content = response.read()
+        s.close()
+        return content
