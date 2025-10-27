1f5970f69952d007ef15a4990724009bf9cf607f
Kresna Satya <kresnasatya72@gmail.com>
2025-10-27 19:26:13 +0800

    Chapter 1: Downloading Web Pages - Displaying the HTML

--- lab01.py
+++ lab01.py
@@ -38,3 +38,30 @@ class URL:
         content = response.read()
         s.close()
         return content
+
+
+def show(body):
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+
+def load(url):
+    body = url.request()
+    show(body)
+
+
+if __name__ == "__main__":
+    import sys
+
+    load(URL(sys.argv[1]))
+
+"""
+To run this program use Python 3:
+    python3 lab01.py http://example.org/
+"""
