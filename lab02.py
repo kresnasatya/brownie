07aa538aec9bd1f53c80db5a478133641130f95d
Kresna Satya <kresnasatya72@gmail.com>
2025-10-28 11:01:49 +0800

    Chapter 2: Drawing to the Screen - Drawing to the Window

---
+++ lab02.py
@@ -0,0 +1,42 @@
+"""
+To make this program runs well, you need to install separated python & python-tk from the OS itself.
+Otherwise you will get the warning message like this:
+    DEPRECATION WARNING: The system version of Tk is deprecated and may be removed in a future release. Please don't rely on it. Set TK_SILENCE_DEPRECATION=1 to suppress this warning.
+It makes the canvas doesn't showing up rectangle, oval, and text.
+To solve this problem, I'm using Homebrew, so the command is this:
+    brew install python python-tk
+Then, export homebrew bin to PATH in order to make separated python & python-tk is used:
+    export PATH="/opt/homebrew/bin:$PATH"
+"""
+
+import tkinter
+from lab01 import show, URL
+
+
+WIDTH, HEIGHT = 800, 600
+
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+
+    def load(self, url):
+        body = url.request()
+        show(body)
+        self.canvas.create_rectangle(10, 20, 400, 300)
+        self.canvas.create_oval(100, 100, 150, 150)
+        self.canvas.create_text(200, 150, text="Hi!")
+
+
+if __name__ == "__main__":
+    import sys
+
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
+
+"""
+To run this program use Python 3:
+    python3 lab02.py https://example.org/
+"""
