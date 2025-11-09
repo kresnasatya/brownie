4e6991282529729d5fde365df014225ea6cabc84
Kresna Satya <kresnasatya72@gmail.com>
2025-11-09 13:01:27 +0800

    Chapter 4: Constructing an HTML Tree - Using the Node Tree

--- lab04.py
+++ lab04.py
@@ -10,11 +10,11 @@ Then, export homebrew bin to PATH in order to make separated python & python-tk 
 """
 
 import tkinter
-import tkinter.font
 
 from lab01 import URL
-from lab02 import HEIGHT, HSTEP, SCROLL_STEP, VSTEP, WIDTH
-from lab03 import get_font
+from lab02 import HSTEP, VSTEP
+from lab03 import Browser as Lab03Browser
+from lab03 import Layout as Lab03Layout
 
 
 class Text:
@@ -133,113 +133,75 @@ class HTMLParser:
         return self.unfinished.pop()
 
 
-class Layout:
-    def __init__(self, tokens):
+class Layout(Lab03Layout):
+    def __init__(self, tree):
         self.display_list = []
         self.cursor_x, self.cursor_y = HSTEP, VSTEP
         self.weight = "normal"
         self.style = "roman"
         self.size = 12
         self.line = []
-        for tok in tokens:
-            self.token(tok)
+        self.recurse(tree)
         self.flush()
 
-    def token(self, tok):
-        if isinstance(tok, Text):
-            for word in tok.text.split():
-                self.word(word)
-        elif tok.tag == "i":
+    def open_tag(self, tag):
+        # print("tag: ", tag)
+        if tag == "i":
             self.style = "italic"
-        elif tok.tag == "/i":
-            self.style = "roman"
-        elif tok.tag == "em":
+        elif tag == "em":
             self.style = "italic"
-        elif tok.tag == "/em":
-            self.style = "roman"
-        elif tok.tag == "b":
+        elif tag == "b":
             self.weight = "bold"
-        elif tok.tag == "/b":
-            self.weight = "normal"
-        elif tok.tag == "strong":
+        elif tag == "strong":
             self.weight = "bold"
-        elif tok.tag == "/strong":
-            self.weight = "normal"
-        elif tok.tag == "small":
+        elif tag == "small":
             self.size -= 2
-        elif tok.tag == "/small":
-            self.size += 2
-        elif tok.tag == "big":
+        elif tag == "big":
             self.size += 4
-        elif tok.tag == "/big":
-            self.size -= 4
-        elif tok.tag == "br":
+        elif tag == "br":
             self.flush()
-        elif tok.tag == "/p":
+
+    def close_tag(self, tag):
+        if tag == "i":
+            self.style = "roman"
+        elif tag == "em":
+            self.style = "roman"
+        elif tag == "b":
+            self.weight = "normal"
+        elif tag == "strong":
+            self.weight = "normal"
+        elif tag == "small":
+            self.size += 2
+        elif tag == "big":
+            self.size -= 4
+        elif tag == "p":
             self.flush()
             self.cursor_y += VSTEP
 
-    def word(self, word):
-        font = get_font(self.size, self.weight, self.style)
-        w = font.measure(word)
-        if self.cursor_x + w > WIDTH - HSTEP:
-            self.flush()
-        self.line.append((self.cursor_x, word, font))
-        self.cursor_x += w + font.measure(" ")
-
-    def flush(self):
-        if not self.line:
-            return
-        metrics = [font.metrics() for x, word, font in self.line]
-        max_ascent = max([metric["ascent"] for metric in metrics])
-        baseline = self.cursor_y + 1.25 * max_ascent
-        for x, word, font in self.line:
-            y = baseline - font.metrics("ascent")
-            self.display_list.append((x, y, word, font))
-        max_descent = max([metric["descent"] for metric in metrics])
-        self.cursor_y = baseline + 1.25 * max_descent
-        self.cursor_x = HSTEP
-        self.line = []
+    def recurse(self, tree):
+        if isinstance(tree, Text):
+            for word in tree.text.split():
+                self.word(word)
+        else:
+            self.open_tag(tree.tag)
+            for child in tree.children:
+                self.recurse(child)
+            self.close_tag(tree.tag)
 
 
-class Browser:
-    def __init__(self):
-        self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
-        self.canvas.pack()
-        self.scroll = 0
-        self.window.bind("<Down>", self.scrolldown)
-
+class Browser(Lab03Browser):
     def load(self, url):
         body = url.request()
-        tokens = lex(body)
-        self.display_list = Layout(tokens).display_list
-        self.draw()
-
-    def draw(self):
-        self.canvas.delete("all")
-        for x, y, word, font in self.display_list:
-            if y > self.scroll + HEIGHT:
-                continue
-            if y + VSTEP < self.scroll:
-                continue
-            self.canvas.create_text(
-                x, y - self.scroll, text=word, font=font, anchor="nw"
-            )
-
-    def scrolldown(self, e):
-        self.scroll += SCROLL_STEP
+        self.nodes = HTMLParser(body).parse()
+        self.display_list = Layout(self.nodes).display_list
         self.draw()
 
 
 if __name__ == "__main__":
     import sys
 
-    # Browser().load(URL(sys.argv[1]))
-    # tkinter.mainloop()
-    body = URL(sys.argv[1]).request()
-    nodes = HTMLParser(body).parse()
-    print_tree(nodes)
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
 
 """
 To run this program use Python 3:
