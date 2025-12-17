2887bcdec7de34fecf914fc8d6695e93d9407552
Kresna Satya <kresnasatya72@gmail.com>
2025-12-17 22:17:44 +0800

    Chapter 8: Sending Information to Servers - Rendering Widgets

--- lab08.py
+++ lab08.py
@@ -274,6 +274,9 @@ class DocumentLayout:
         child.layout()
         self.height = child.height
 
+    def should_paint(self):
+        return True
+
     def paint(self):
         return []
 
@@ -399,8 +402,29 @@ class BlockLayout:
         else:
             if node.tag == "br":
                 self.flush()
-            for child in node.children:
-                self.recurse(child)
+            elif node.tag == "input" or node.tag == "button":
+                self.input(node)
+            else:
+                for child in node.children:
+                    self.recurse(child)
+
+    def input(self, node):
+        w = INPUT_WIDTH_PX
+        if self.cursor_x + w > self.width:
+            self.new_line()
+        line = self.children[-1]
+        previous_word = line.children[-1] if line.children else None
+        input = InputLayout(node, line, previous_word)
+        line.children.append(input)
+
+        weight = node.style["font-weight"]
+        style = node.style["font-style"]
+        if style == "normal":
+            style = "roman"
+        size = int(float(node.style["font-size"][:-2]) * 0.75)
+        font = get_font(size, weight, style)
+
+        self.cursor_x += w + font.measure(" ")
 
     def layout_intermediate(self):
         previous = None
@@ -419,10 +443,15 @@ class BlockLayout:
             ]
         ):
             return "block"
-        elif self.node.children:
+        elif self.node.children or self.node.tag == "input":
             return "inline"
         else:
             return "block"
+
+    def should_paint(self):
+        return isinstance(self.node, Text) or (
+            self.node.tag != "input" and self.node.tag != "button"
+        )
 
 
 class LineLayout:
@@ -454,6 +483,9 @@ class LineLayout:
     def paint(self):
         return []
 
+    def should_paint(self):
+        return True
+
 
 class TextLayout:
     def __init__(self, node, word, parent, previous):
@@ -481,6 +513,69 @@ class TextLayout:
     def paint(self):
         color = self.node.style["color"]
         return [DrawText(self.x, self.y, self.word, self.font, color)]
+
+    def should_paint(self):
+        return True
+
+
+INPUT_WIDTH_PX = 200
+
+
+class InputLayout:
+    def __init__(self, node, parent, previous) -> None:
+        self.node = node
+        self.children = []
+        self.parent = parent
+        self.previous = previous
+        self.x = None
+        self.y = None
+        self.width = None
+        self.height = None
+        self.font = None
+
+    def layout(self):
+        weight = self.node.style["font-weight"]
+        style = self.node.style["font-style"]
+        if style == "normal":
+            style = "roman"
+        size = int(float(self.node.style["font-size"][:2]) * 0.75)
+        self.font = get_font(size, weight, style)
+        self.width = INPUT_WIDTH_PX
+        if self.previous:
+            space = self.previous.font.measure(" ")
+            self.x = self.previous.x + space + self.previous.width
+        else:
+            self.x = self.parent.x
+        self.height = self.font.metrics("linespace")
+
+    def should_paint(self):
+        return True
+
+    def paint(self):
+        cmds = []
+        bgcolor = self.node.style.get("background-color", "transparent")
+        if bgcolor != "transparent":
+            rect = DrawRect(self.self_rect(), bgcolor)
+            cmds.append(rect)
+        text = ""
+        if self.node.tag == "input":
+            text = self.node.attributes.get("value", "")
+        elif self.node.tag == "button":
+            if len(self.node.children) == 1 and isinstance(self.node.children[0], Text):
+                text = self.node.children[0].text
+            else:
+                print("Ignoring HTML contents inside button")
+        color = self.node.style["color"]
+        cmds.append(DrawText(self.x, self.y, text, self.font, color))
+        return cmds
+
+    def self_rect(self):
+        return Rect(
+            left=self.x,
+            top=self.y,
+            right=self.x + self.width,
+            bottom=self.y + self.height,
+        )
 
 
 class DrawText:
@@ -520,7 +615,8 @@ class DrawRect:
 
 
 def paint_tree(layout_object, display_list):
-    display_list.extend(layout_object.paint())
+    if layout_object.should_paint():
+        display_list.extend(layout_object.paint())
 
     for child in layout_object.children:
         paint_tree(child, display_list)
