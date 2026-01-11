6681f8002734b41d021db1965f6ef8da07cae516
Kresna Satya <kresnasatya72@gmail.com>
2026-01-11 11:31:15 +0800

    merge TagSelector and DescendantSelector classes into CSSParser class

--- css_parser.py
+++ css_parser.py
@@ -1,4 +1,4 @@
-from tag_selector import TagSelector
+from element import Element
 
 class CSSParser:
     def __init__(self, s) -> None:
@@ -88,3 +88,26 @@ class CSSParser:
                 else:
                     break
         return rules
+
+class TagSelector:
+    def __init__(self, tag):
+        self.tag = tag
+        self.priority = 1
+
+    def matches(self, node):
+        return isinstance(node, Element) and self.tag == node.tag
+
+class DescendantSelector:
+    def __init__(self, ancestor, descendant) -> None:
+        self.ancestor = ancestor
+        self.descendant = descendant
+        self.priority = ancestor.priority + descendant.priority
+
+    def matches(self, node):
+        if not self.descendant.matches(node):
+            return False
+        while node.parent:
+            if self.ancestor.matches(node.parent):
+                return True
+            node = node.parent
+        return False
