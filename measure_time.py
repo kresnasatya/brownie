e71666b3f43ba466e2499155ac19c02c663628b0
Kresna Satya <kresnasatya72@gmail.com>
2026-02-09 22:54:19 +0800

    Chapter 12: Scheduling Tasks and Threads - Threaded Profiling

--- measure_time.py
+++ measure_time.py
@@ -1,7 +1,9 @@
 import time
+import threading
 
 class MeasureTime:
     def __init__(self):
+        self.lock = threading.Lock()
         self.file = open("browser.trace", "w")
         self.file.write('{"traceEvents": [')
         ts = time.time() * 1_000_000
@@ -15,25 +17,39 @@ class MeasureTime:
         self.file.flush()
 
     def time(self, name):
+        self.lock.acquire(blocking=True)
         ts = time.time() * 1_000_000
+        tid = threading.get_ident()
         self.file.write(
             ', { "ph": "B", "cat": "_",' +
             '"name": "' + name + '",' +
             '"ts": ' + str(ts) + ',' +
-            '"pid": 1, "tid": 1}'
+            '"pid": 1, "tid": ' + str(tid) + '}'
         )
         self.file.flush()
+        self.lock.release()
 
     def stop(self, name):
+        self.lock.acquire(blocking=True)
         ts = time.time() * 1_000_000
+        tid = threading.get_ident()
         self.file.write(
             ', { "ph": "E", "cat": "_",' +
             '"name": "' + name + '",' +
             '"ts": ' + str(ts) + ',' +
-            '"pid": 1, "tid": 1}'
+            '"pid": 1, "tid": ' + str(tid) + '}'
         )
         self.file.flush()
+        self.lock.release()
 
     def finish(self):
+        self.lock.acquire(blocking=True)
+        for thread in threading.enumerate():
+            self.file.write(
+                ', { "ph": "M", "name": "thread_name",' +
+                '"pid": 1, "tid": ' + str(thread.ident) + ',' +
+                '"args": { "name": "' + thread.name + '"}}'
+            )
         self.file.write(']}')
         self.file.close()
+        self.lock.release()
