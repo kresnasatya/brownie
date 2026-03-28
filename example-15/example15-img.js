d711931161adba6ba2497030b49e6dcd03bc1e29
Kresna Satya <halo@kresna.me>
2026-03-28 19:21:16 +0800

    Add example-15 static site for test iframe

---
+++ example-15/example15-img.js
@@ -0,0 +1,11 @@
+window.document
+  .querySelectorAll("img")[0]
+  .addEventListener("click", function (e) {
+    window.console.log("clicked");
+    e.preventDefault();
+  });
+
+var parentWindow = window.parent;
+
+if (parentWindow)
+  parentWindow.postMessage("This is the contents of postMessage.", "*");
---
+++ example-15/iframe.js
@@ -0,0 +1,7 @@
+window.addEventListener(
+  "message",
+  function (e) {
+    window.console.log("Message received from iframe: " + e.data);
+  },
+  false
+);
