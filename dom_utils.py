c69f644f449b97e60094ed92eb986423fbe50945
Kresna Satya <halo@kresna.me>
2026-04-05 10:49:42 +0800

    fix: set default value size to None in font() in dom_utils

--- dom_utils.py
+++ dom_utils.py
@@ -258,6 +258,7 @@ def speak_text(text):
 def font(style, zoom):
     weight = style["font-weight"]
     variant = style["font-style"]
+    size = None
     try:
         size = float(style["font-size"][:-2]) * 0.75
     except ValueError:
